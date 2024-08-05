f338beead7d2c7045dc26926acd47d8d78a8f72c
Nicholas Matsumoto <nk.matsumoto@gmail.com>
2024-08-05 22:11:42 +0900

    Code along for lecture

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,21 @@ Rails.application.routes.draw do
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  #
 
+  get "up" => "rails/health#show", as: :rails_health_check
+  # Read all
+  get "restaurants", to: "restaurants#index"
+  # Create
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
+  # Read one
+  get "restaurants/:id", to: "restaurants#show", as: :restaurant
+  # Update
+  get "restaurants/:id/edit", to: "restaurants#edit"
+  patch "restaurants/:id", to: "restaurants#update"
+  # Delete
+  delete "restaurants/:id", to: "restaurants#destroy"
   # Defines the root path route ("/")
   # root "posts#index"
 end
